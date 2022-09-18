a4a6fe169793e17b64703c005e32c151a369c148
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-09-18 15:06:26 -0400

    html & api routes - no clue if this works

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ SO THAT I can organize my thoughts and keep track of tasks I need to complete
 To install run 
 ```
 npm install express
+npm install uuid
 ```
 
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,22 @@
 const router = require('express').Router();
+const db = require('../db/db.json');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+let id = '';
+
+router.get('/api/notes', (req, res) => {
+    res.json(db);
+});
+
+router.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    db.push(req.body);
+
+    fs.write('./db/db.json', JSON.stringify(db), function (err) {
+        if (err) console.log("Error writing file:", err);
+        res.json(db);
+        })
+});
+
+module.exports = router;
