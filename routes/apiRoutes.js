7db6516fb0c8b97e413bed4a5d3bc35a7df5dd9b
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-09-18 20:19:36 -0400

    changed code back to a previous commit & found error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,23 @@
 const router = require('express').Router();
 const db = require('../db/db.json');
 const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+
+let id = '';
 
 //`GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 router.get('/notes', (req, res) => {
-    fs.readFile(db, 'utf8', (error, data) => {
-        if(error){
-            console.log(error);
-            return;
-        }
-        res.json(db);
-    })
+    res.json(db);
 });
 
 router.post('/notes', (req, res) => {
     req.body.id = uuidv4();
     db.push(req.body);
 
-    fs.write('./db/db.json', JSON.stringify(db), function (err) {
+    fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
         if (err) console.log("Error writing file:", err);
         res.json(db);
-        })
+    })
 });
-
 module.exports = router;
--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const htmlRoute = require('./routes/htmlRoutes');
 const PORT = process.env.port || 3001;
 const app = express();
 
+//Need access ot public Folder before routes
+app.use(express.static('public'));
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoute);
 app.use('/', htmlRoute);
 
-app.use(express.static('public'));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
