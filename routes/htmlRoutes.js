2dfa067c29f4dadf76e02b824649144d0ccf8b34
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-09-21 19:21:27 -0400

    testing routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,17 @@ const path = require("path");
 
 // Route to serve up the /public/index.html page or the Home Page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
 // Route to serve up the /public/notes.html page
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // Route to serve up the /public/index.html page for any wildcards (*)
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
 module.exports = router;
