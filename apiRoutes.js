db79742cb54ae4b2c54aa78c3ef90022c2b93b46
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-09-21 19:16:42 -0400

    It's finally workiung

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const db = require('./db/db.json');
+const db = require('./db/db.json'); //Not Live
 const util = require('util');
 const fs = require('fs');
 // UUID (NPM Package) universally unique identifier 
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 
 // GET Method - Reading db.json file & returning in JSON format
 router.get('/notes', (req, res) => {
-    //Stephen said this method won't work b/c by the time you push into db the get will be outdated.
+    //Stephen said this method won't work b/c by the time you post into db the get will be outdated.
     res.json(db);
     //said to use this for delete method:
     //fs.readFile('db/db.json', (err, data) => err ? res.status(500).json(err) : res.json(data));
@@ -28,38 +28,13 @@ router.post('/notes', (req, res) => {
 });
 
 // BONUS TO DO: router.delete()
-// router.delete('/notes/:id', (req, res) => {
-//     //define the dbID varaible as the id parameter
-//     //var dbID = JSON.parse(req.params.id);
-//     var DBid = parseInt(req.params.id)
-
-//     for (var i = 0; i < db.length; i++) {
-//         if (db[i].DBid === DBid) {
-//             db.splice(i, 1);
-//         }
-//     }
-
-//     //rewrite the db.json without the note removed
-//     fs.writeFile('../db/db.json', JSON.stringify(db), function (err) {
-//         if (err) throw error;
-//         res.json(db);
-//     })
-// }) 
-
-//read notes
-//filter through notes
-//.then() pass through info
-//.filter(filter for the deleted note id) - goes through each on 
-//needs to pass through test 
-//compare id to deleted notes id
-//write back to db json file
-
-// for (let i=0; i < db.length; i++) {
-//     if(db[i].id === id) {
-//         db.splice(i, 1);
-//         console.log("Note deleted");
-//     }
-// }; 
-// return;
+router.delete('/notes/:id', (req, res) => {
+    //define the dbID varaible as the id parameter
+    const DBid = req.params.id;
+    db.splice(DBid, 1);
+    //rewrite the db.json without the note removed
+    fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 4));
+    res.json(req.body);
+})
 
 module.exports = router;
